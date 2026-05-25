702a596ec728034db131eba38575f59f86d430c5
Jonah987654321 <jonah.emme@web.de>
2026-05-25 16:40:24 +0200

    feat: main loop and exit condition

--- scriptor.asm
+++ scriptor.asm
@@ -3,11 +3,15 @@ section .bss
     termios_orig    resb 60
     termios_raw     resb 60
 
+    ; --- One byte buffer for inputs
+    input_buf       resb 1
+
 section .text
 global _start
 
 _start:
     call raw_mode_enter
+    call loop
     call raw_mode_exit
     call exit
 
@@ -26,9 +30,10 @@ raw_mode_enter:
     mov rcx, 60
     rep movsb
 
-    ; --- Unset ICANON and ECHO flags
+    ; --- Disable ISIG, ICANON and ECHO in c_lflag and IXOFF in c_iflag
     ; https://www.man7.org/linux/man-pages/man3/termios.3.html
-    and dword [termios_raw + 12], ~0x0A
+    and dword [termios_raw + 12], ~0x0B
+    and dword [termios_raw + 0], ~0x400
 
     ; --- Set the modified state by calling ioctl again
     mov rax, 16
@@ -50,7 +55,33 @@ raw_mode_exit:
     ret
 
 exit:
-    ; --- Set exit code 0 (by XORing) and call exit syscall
+    ; --- Set exit code 0 (by XORing) and call exit (syscall 60)
     mov rax, 60
     xor rdi, rdi
     syscall
+
+loop:
+    ; --- Read one byte from stdin (syscall 0)
+    ; https://www.man7.org/linux/man-pages/man2/read.2.html
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input_buf
+    mov rdx, 1
+    syscall
+
+    ; --- Check for Ctrl + Q (0x11 ascii) for exit
+    mov al, [input_buf]
+    cmp al, 0x11
+    je .exit
+
+    ; --- Print everything else for now (syscall 1 -> write)
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, input_buf
+    mov rdx, 1
+    syscall
+
+    jmp loop 
+
+    .exit:
+        ret
